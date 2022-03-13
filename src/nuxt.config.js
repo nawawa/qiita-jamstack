b925d5587034cefcc86c6e26de6504d65859b723
nawawa <oonawa12@gmail.com>
2022-03-13 21:15:36 +0900

    サイトタイトル仮決め

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'nawawa',
+    titleTemplate: 'nawawa - %s',
     title: 'nawawa',
     htmlAttrs: {
       lang: 'en'
