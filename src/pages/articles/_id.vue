7bdc25a17c642ec9aa755c75d58d37caa1c2d5dd
nawawa <oonawa12@gmail.com>
2022-03-18 13:48:23 +0900

    記事HTML展開・コードブロック部分の成型

--- src/pages/articles/_id.vue
+++ src/pages/articles/_id.vue
@@ -1,8 +1,10 @@
 <template>
-  <main>
-    <h1>{{title}}</h1>
-    <div>{{body}}</div>
-  </main>
+  <article>
+    <header>
+      <h1>{{title}}</h1>
+    </header>
+    <div v-html='rendered_body' />
+  </article>
 </template>
 
 <script>
@@ -17,6 +19,76 @@ export default {
       }
     )
     return data
+  },
+  computed: {
+    stylingCodeBlock() {
+      return function(elements, styles) {
+          Array.prototype.forEach.call(elements, function(el) {
+          el.style = styles;
+        });
+      };
+    }
+  },
+  mounted() {
+
+    this.stylingCodeBlock(
+      document.getElementsByClassName('code-frame'), 
+      `
+        background-color: #364549;
+        color: #e3e3e3;
+        margin: 1.5em -32px;
+        padding: 1em 32px;
+        font-size: .9em;
+        position: relative;
+      `
+    );
+
+    this.stylingCodeBlock(
+      document.getElementsByTagName('pre'), 
+      `
+        display: block;
+        padding: 0;
+        margin: 0;
+        font-size: inherit;
+        font-family: "SFMono-Regular",Consolas,"Liberation Mono",Menlo,Courier,monospace;
+        line-height: inherit;
+        color: inherit;
+        background-color: transparent;
+        border: none;
+        border-radius: 0;
+        line-height: 1.8;
+      `
+    );
+
+    this.stylingCodeBlock(
+      document.getElementsByClassName('highlight'), 
+      `
+        padding: .5em;
+        overflow-x: auto;
+      `
+    );
+
+    this.stylingCodeBlock(
+      document.querySelectorAll('pre > code'), 
+      `
+        display: block;
+        width: -webkit-fit-content;
+        width: -moz-fit-content;
+        width: fit-content;
+        min-width: 100%;
+        -webkit-text-size-adjust: 100%;
+        background-color: inherit;
+        color: inherit;
+        font-family: inherit;
+        padding: inherit;
+      `
+    );
+
+    const span_c_elements = document.getElementsByClassName('c');
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
