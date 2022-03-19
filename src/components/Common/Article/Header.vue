0edf2fc648ec45fc3dae013b7f0c8dbd927b93d6
nawawa <oonawa12@gmail.com>
2022-03-19 12:08:30 +0900

    投稿日・更新日の表示

--- src/components/Common/Article/Header.vue
+++ src/components/Common/Article/Header.vue
@@ -1,5 +1,33 @@
 <template>
   <header>
+    <CommonArticleTimeStamp>
+      <template #type>
+        <slot name="type_create" />
+      </template>
+      <template #year>
+        <slot name="c_year" />
+      </template>
+      <template #month>
+        <slot name="c_month" />
+      </template>
+      <template #date>
+        <slot name="c_date" />
+      </template>
+    </CommonArticleTimeStamp>
+    <CommonArticleTimeStamp>
+      <template #type>
+        <slot name="type_update" />
+      </template>
+      <template #year>
+        <slot name="u_year" />
+      </template>
+      <template #month>
+        <slot name="u_month" />
+      </template>
+      <template #date>
+        <slot name="u_date" />
+      </template>
+    </CommonArticleTimeStamp>
     <CommonArticleTitle>
       <slot name="title" />
     </CommonArticleTitle>
--- src/pages/articles/_id.vue
+++ src/pages/articles/_id.vue
@@ -1,8 +1,15 @@
 <template>
   <article>
-    <p>{{ user_id }}</p>
     <CommonArticleHeader>
-      <template v-slot:title>{{article.title}}</template>
+      <template #type_create>{{'投稿日'}}</template>
+      <template #c_year>{{created_at.year}}</template>
+      <template #c_month>{{created_at.month}}</template>
+      <template #c_date>{{created_at.date}}</template>
+      <template #type_update>{{'更新日'}}</template>
+      <template #u_year>{{updated_at.year}}</template>
+      <template #u_month>{{updated_at.month}}</template>
+      <template #u_date>{{updated_at.date}}</template>
+      <template #title>{{article.title}}</template>
     </CommonArticleHeader>
     <CommonArticleBody>
       <div v-html='article.rendered_body' />
@@ -14,6 +21,20 @@
 import axios from 'axios'
 
 export default {
+  data() {
+    return {
+      created_at: {
+        year: '',
+        month: '',
+        date: '',
+      },
+      updated_at: {
+        year: '',
+        month: '',
+        date: '',
+      }
+    }
+  },
   async asyncData({ params, $config: { apiSecret, apiURL }, redirect }) {
     const {data} = await axios.get(
       `${apiURL}/items/${params.id}`,
@@ -22,8 +43,27 @@ export default {
       }
     )
     return (data.user.id === 'inarikawa') ?
-      { article: data, user_id: data.user.id }:
+      { article: data }:
       redirect({ path: '/404'});
   },
+  mounted() {
+    const created_at = this.createDateObject(new Date(this.article.created_at));
+    this.created_at.year = created_at.year;
+    this.created_at.month = created_at.month;
+    this.created_at.date = created_at.date;
+    const updated_at = this.createDateObject(new Date(this.article.updated_at));
+    this.updated_at.year = updated_at.year;
+    this.updated_at.month = updated_at.month;
+    this.updated_at.date = updated_at.date;
+  },
+  methods: {
+    createDateObject(date_object) {
+      return {
+        year: date_object.getFullYear(),
+        month: date_object.getMonth() + 1,
+        date: date_object.getDate()
+      };
+    },
+  }
 }
 </script>
