d4c047a289a7a937df2df61fb40734e77118a524
nawawa <oonawa12@gmail.com>
2022-03-26 19:00:10 +0900

    設定部分のコメントを調整

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -13,9 +13,8 @@ export default {
     apiSecret: process.env.NODE_ENV !== 'production' ? process.env.API_SECRET : ''
   },
 
-  // 動的ルーティングパスの設定
   generate: {
-    fallback: true,
+    fallback: true, // 不明なパスへのアクセス時もNuxtコンポーネントで解決
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
