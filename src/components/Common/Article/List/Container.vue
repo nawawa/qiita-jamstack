5d2b7b1a2ca4583df824a26d6863999aa8078f63
nawawa <oonawa12@gmail.com>
2022-03-28 14:35:18 +0900

    記事を全て取得、繰り返し文でv-cardコンポーネントへ流し込みする実装

---
+++ src/components/Common/Article/List/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="article in articles" :key="article.id">
+      <NuxtLink :to="`/articles/${article.id}`">
+        <CommonArticleCard>
+          {{ article.title }}
+        </CommonArticleCard>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  props: {
+    articles: {
+      type: Array
+    }
+  }
+}
+</script>
