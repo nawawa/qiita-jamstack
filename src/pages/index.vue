90de16bd4bf5cb0ea337fd971ed4ec259aa10362
nawawa <oonawa12@gmail.com>
2022-03-29 09:11:21 +0900

    Qiitaからユーザー情報を取得してstoreする一連の処理を実装

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,7 @@
     <CommonArticleListContainer :articles="articles" />
     <NuxtLink to="/articles/page/2">2</NuxtLink>
     <NuxtLink to="/articles/page/3">3</NuxtLink>
+    {{user.items_count}}
   </v-container>
 </template>
 
@@ -10,14 +11,29 @@
 import axios from 'axios';
 
 export default {
+  data() {
+    return {
+      articles: {},
+      user_data: {}
+    }
+  },
+  mounted() {
+    this.$store.commit('user/add', this.user_data);
+  },
+  computed: {
+    user() {
+      return this.$store.state.user.user
+    }
+  },
   async asyncData({ $config: { apiSecret, apiURL } }) {
-    const {data} = await axios.get(
-      `${apiURL}/users/inarikawa/items?page=1&per_page=10`,
-      {
-        headers: { Authorization: `Bearer ${apiSecret}` }
-      }
-    )
-    return { articles: data };
+    const token = { Authorization: `Bearer ${apiSecret}` };
+
+    const response = await Promise.all([
+      axios.get(`${apiURL}/users/inarikawa/items?page=1&per_page=10`, { headers: token }),
+      axios.get(`${apiURL}/users/inarikawa`, { headers: token }),
+    ]);
+
+    return { articles: response[0].data, user_data: response[1].data };
   },
 }
 </script>
