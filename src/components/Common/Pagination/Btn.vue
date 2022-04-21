9471630f8584669bdf938af5cf67326b14808b09
nawawa <oonawa12@gmail.com>
2022-04-22 08:24:19 +0900

    自前実装に変え始め

--- src/components/Common/Pagination/Btn.vue
+++ src/components/Common/Pagination/Btn.vue
@@ -1,19 +1,26 @@
 <template>
-  <div>
-    {{ page }}
-  </div>
+  <v-col>
+    <div>
+      {{ page_number }}
+    </div>
+  </v-col>
 </template>
 
 <script>
 export default {
   props: {
-    page: Number,
+    page_number: Number,
   },
 }
 </script>
 
 <style lang="scss" scoped>
   div {
-    
+    width: 20px;
+    height: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
   }
 </style>
--- src/components/Layouts/Pagination.vue
+++ src/components/Layouts/Pagination.vue
@@ -1,7 +1,7 @@
 <template>
   <CommonPaginationContainer>
     <NuxtLink v-for="number in length" :key="number" :to="`/articles/page/${number}`">
-      <CommonPaginationBtn :page="number" />
+      <CommonPaginationBtn :page_number="number" />
     </NuxtLink>
   </CommonPaginationContainer>
 </template>
