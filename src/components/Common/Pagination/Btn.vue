66cf92167a89c317521bc81ef9748ad16f52a56a
nawawa <oonawa12@gmail.com>
2022-04-22 08:05:13 +0900

    v-pagination を使いつつSSGを使うのは無理……？

--- src/components/Common/Pagination/Btn.vue
+++ src/components/Common/Pagination/Btn.vue
@@ -1,18 +1,19 @@
 <template>
-  <v-pagination
-    v-model="page"
-    :length="length"
-    circle
-  >
-    <nuxt-link v-for="number in length" :key="number" :to="`/articles/page/${number}`" />
-  </v-pagination>
+  <div>
+    {{ page }}
+  </div>
 </template>
 
 <script>
 export default {
   props: {
     page: Number,
-    length: Number
   },
 }
 </script>
+
+<style lang="scss" scoped>
+  div {
+    
+  }
+</style>
--- src/components/Layouts/Pagination.vue
+++ src/components/Layouts/Pagination.vue
@@ -1,10 +1,16 @@
 <template>
   <CommonPaginationContainer>
-    <CommonPaginationBtn v-bind="$attrs" />
+    <NuxtLink v-for="number in length" :key="number" :to="`/articles/page/${number}`">
+      <CommonPaginationBtn :page="number" />
+    </NuxtLink>
   </CommonPaginationContainer>
 </template>
 
 <script>
 export default {
+  props: {
+    page: Number,
+    length: Number
+  },
 }
 </script>
