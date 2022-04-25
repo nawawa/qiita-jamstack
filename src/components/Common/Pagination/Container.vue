97192fd4e45ddaf71192742441ecf1faf8246c6d
nawawa <oonawa12@gmail.com>
2022-04-26 07:49:17 +0900

    ページネーションボタン：左右の余白を削除

--- src/components/Common/Pagination/Container.vue
+++ src/components/Common/Pagination/Container.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="my-12">
     <slot />
   </div>
 </template>
@@ -11,7 +11,6 @@ export default {
 
 <style lang="scss" scoped>
   div {
-    margin: 50px 20%;
     text-align: center;
   }
 </style>
