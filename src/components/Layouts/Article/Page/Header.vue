19a8cd224b1d1247a819ff8655900afe52b62e6f
nawawa <oonawa12@gmail.com>
2022-04-27 08:19:47 +0900

    新たにコンポーネント分け

---
+++ src/components/Layouts/Article/Page/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <CommonArticleHeader>
+    <CommonArticleTitle :title="title" />
+    <CommonArticleInfo :created_at="created_at" :updated_at="updated_at" />
+  </CommonArticleHeader>
+</template>
+
+<script>
+export default {
+  props: {
+    created_at: Object,
+    updated_at: Object,
+    title: String
+  }
+}
+</script>
